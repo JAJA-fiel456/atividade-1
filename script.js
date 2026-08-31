38ea4b10f59bdca10be7f4d9e5e2c7c48ff52d9c
JAJA-fiel456 <joaojacomo341@gmail.com>
2026-08-31 17:49:24 +0000

    Atualiza página inicial

---
+++ script.js
@@ -0,0 +1,35 @@
+const famosos = {
+    einstein: {
+        nome: "Albert Einstein",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/500px-Albert_Einstein_Head.jpg"
+    },
+    monroe: {
+        nome: "Marilyn Monroe",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Marilyn_Monroe_1953_studio_portrait.jpg/440px-Marilyn_Monroe_1953_studio_portrait.jpg"
+    },
+    turing: {
+        nome: "Alan Turing",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Alan_Turing_Aged_16.jpg/440px-Alan_Turing_Aged_16.jpg"
+    },
+    frida: {
+        nome: "Frida Kahlo",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Frida_Kahlo%2C_by_Guillermo_Kahlo.jpg/440px-Frida_Kahlo%2C_by_Guillermo_Kahlo.jpg"
+    },
+    pele: {
+        nome: "Pelé",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Pele_con_brasil_%28cropped%29.jpg/440px-Pele_con_brasil_%28cropped%29.jpg"
+    },
+    curie: {
+        nome: "Marie Curie",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Marie_Curie_c._1920s.jpg/440px-Marie_Curie_c._1920s.jpg"
+    },
+    jobs: {
+        nome: "Steve Jobs",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/440px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg"
+    },
+    mandela: {
+        nome: "Nelson Mandela",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nelson_Mandela_1994.jpg/440px-Nelson_Mandela_1994.jpg"
+    }
+};
+
